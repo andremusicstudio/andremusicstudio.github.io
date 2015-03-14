9e99a6504007ae4e8d89450a337adf6aee389747
Boris Ayupov <boris.ayupov@gmail.com>
2015-03-13 19:28:04 -0700

    Add google map

---
+++ js.js
@@ -0,0 +1,26 @@
+/**
+ * Created by borisayupov on 3/13/15.
+ */
+window.onload = function () {
+
+    var firstLatlng = new google.maps.LatLng(37.326650, -121.938010);
+    var secondLatlng = new google.maps.LatLng(37.321235, -122.054849);
+    var canvasCenter = new google.maps.LatLng(37.351235, (-121.938010-122.054849)/2);
+    var mapOptions = {
+        zoom: 10,
+        center: canvasCenter
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    // To add the marker to the map, use the 'map' property
+    var marker = new google.maps.Marker({
+        position: firstLatlng,
+        map: map,
+        title: "120 Ciro Ave, San Jose, CA"
+    });
+    var marker2 = new google.maps.Marker({
+        position: secondLatlng,
+        map: map,
+        title: "10141'A' Pasadena Ave, Cupertino, CA"
+    });
+}
